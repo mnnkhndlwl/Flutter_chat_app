28432ea5cf02fa05b8a7ebc2ddf83db63266be2c
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-09-05 23:36:00 +0530

    succesfully coonected to firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (ctx, index) => Container(
-                padding: EdgeInsets.all(10),
-                child: Text('this works'),
-              )),
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: EdgeInsets.all(10),
+          child: Text('this works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('/chats/UiV8pJpaopKdQZtdXL5l/messages/')
+              .snapshots().listen((data) {
+                print(data);
+              });
+        },
+      ),
     );
   }
 }
